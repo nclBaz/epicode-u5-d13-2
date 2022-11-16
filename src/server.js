b65f3fc99c29aa9a566502cccf2ee72fb16152a1
Riccardo Gulin <riccardo@nucleode.com>
2022-11-16 11:53:08 +0100

    Adds basic socket.io config

--- src/server.js
+++ src/server.js
@@ -1,9 +1,15 @@
 import express from "express"
 import listEndpoints from "express-list-endpoints"
 import mongoose from "mongoose"
+import { Server as SocketIOServer } from "socket.io"
+import { createServer } from "http" // CORE MODULE
 
 const expressServer = express()
 const port = process.env.PORT || 3001
+
+// **************************** SOCKETIO ******************
+const httpServer = createServer(expressServer)
+new SocketIOServer(httpServer) // this constructor is expecting to receive an HTTP-SERVER not an EXPRESS SERVER!!
 
 // *********************** MIDDLEWARES ********************
 
